aed8074cf78c3572d07433564381c6f445cf6ed3
Martin <juanmartinhoyoscoronado@gmail.com>
2025-02-06 23:03:18 +0100

    cambios y menu admin

---
+++ admin_login.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+$servername = "bew3kbjtj9n5faq31kla-mysql.services.clever-cloud.com";
+$username = "ueaxccosiwgfnuo5";
+$password = "J9d5wTPIyWsgRyXmEJfd";
+$dbname = "bew3kbjtj9n5faq31kla";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Error en la conexión: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['correo'];
+    $contrasenia = $_POST['password'];
+
+    $stmt = $conn->prepare("SELECT * FROM Administrador WHERE email = ? AND contrasenia = ?");
+    $stmt->bind_param("ss", $email, $contrasenia);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        // Se obtiene la información del usuario
+        $row = $result->fetch_assoc();
+        $_SESSION['usuario'] = $email;
+        $_SESSION['nombre'] = $row['nombre']; // Guarda el nombre obtenido de la BD
+        header("Location: Menu_administrador.php"); // Redirige a la página principal (asegúrate de que la extensión sea .php)
+        exit();
+    } else {
+        echo "<script>alert('Por favor dejar de intentar si no tiene cuenta administrador.'); window.location.href='admin_login.html.html';</script>";
+    }
+    $stmt->close();
+}
+
+$conn->close();
