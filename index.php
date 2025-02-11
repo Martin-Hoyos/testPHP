bcc5c6d4b6b87544fcd7ecfa6ce313d2956a72dd
Martin <juanmartinhoyoscoronado@gmail.com>
2025-02-11 12:49:51 +0100

    cambios

--- index.php
+++ index.php
@@ -138,7 +138,7 @@ session_start();
                         </h2>
                         <div class="flex space-x-2 mt-2">
                             <button class="bg-transparent border border-white text-white py-1 px-4">
-                                <a href="/HOTELES/html/Habitaciones/suitequeen.jsp" id="foto-Vista-Rapida">Vista rápida</a>
+                                <a href="suitQueen.html" id="foto-Vista-Rapida">Vista rápida</a>
 
                             </button>
                             <button id="foto-Reservar-ahora" class="bg-white text-black py-1 px-4">
@@ -162,7 +162,7 @@ session_start();
                         </h2>
                         <div class="flex space-x-2 mt-2">
                             <button class="bg-transparent border border-white text-white py-1 px-4">
-                                <a href="/HOTELES/html/Habitaciones/suiteking.jsp" id="foto-Vista-Rapida">Vista rápida</a>
+                                <a href="SuitKing.html" id="foto-Vista-Rapida">Vista rápida</a>
 
                             </button>
                             <button id="foto-Reservar-ahora" class="bg-white text-black py-1 px-4">
@@ -186,7 +186,7 @@ session_start();
                         </h2>
                         <div class="flex space-x-2 mt-2">
                             <button class="bg-transparent border border-white text-white py-1 px-4">
-                                <a href="..Habitaciones/suitekingbalcon.jsp" id="foto-Vista-Rapida">Vista rápida</a>
+                                <a href="SUITEKINGBALCÓN.html" id="foto-Vista-Rapida">Vista rápida</a>
                             </button>
                             <button id="foto-Reservar-ahora" class="bg-white text-black py-1 px-4">
                                 <a href="Reservar.php">Reservar ahora</a>
