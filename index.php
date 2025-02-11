d3b442f9f18ec41b48152222e49f3f6c27b0f1c7
Martin <juanmartinhoyoscoronado@gmail.com>
2025-02-11 19:43:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ session_start();
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"/>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"/>
+    <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 </head>
 <body>
 
